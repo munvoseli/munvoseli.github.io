29e2f484e3bb3079e3fc5c1fe363b8516b4232b3
munvoseli <muslinmelody@gmail.com>
2021-01-22 21:43:38 -0500

    modified:   wykoff.js

--- wykoff.js
+++ wykoff.js
@@ -229,7 +229,7 @@ function updateTouches(touchList)
       ctx.fillText(lastPitches.length, 20, 80);*/
     lastPitches = nowPitches;
 }
-document.addEventListener("touchstart", function(e) {e.preventDefault();}, false); // for preventing default
+document.body.addEventListener("touchstart", function(e) {e.preventDefault(); e.stopPropogation();}, false); // for preventing default
 canvas.addEventListener("touchstart", function(e) {
     e.preventDefault();
     updateTouches(e.touches);
